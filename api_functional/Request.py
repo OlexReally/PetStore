cc7192487064596c84c41d00b9e8149dbf893e13
AndriiKravtsov <andrjusyk@gmail.com>
2017-12-18 14:48:56 +0200

    Change getapi

--- api_functional/Request.py
+++ api_functional/Request.py
@@ -1,8 +1,7 @@
 import requests
 
 
-def getapi(url, headerskeys):
-    headers = {headerskeys[0]: headerskeys[1]}
-    return requests.get(url, headers=headers)
+def getapi(url):
+    return requests.get(url, headers={'accept': 'application/xml'})
 
 
