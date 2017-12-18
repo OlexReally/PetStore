3ea35bec141cfa5736f5bae98e5c98762ebe3490
AndriiKravtsov <andrjusyk@gmail.com>
2017-12-18 16:14:41 +0200

    Post request

--- api_functional/Request.py
+++ api_functional/Request.py
@@ -2,6 +2,18 @@ import requests
 
 
 def getapi(url):
-    return requests.get(url, headers={'accept': 'application/xml'})
+    answer = requests.get(url, headers={'accept': 'application/xml'})
+    if answer.status_code == 200:
+        return answer
+    elif answer.status_code == 400:
+        raise RuntimeError('Invalid ID supplied')
+    elif answer.status_code == 404:
+        raise RuntimeError('Pet not found')
 
-
+def post_add_pet(dataxml):
+    answer = requests.get('http://petstore.swagger.io/v2/pet', data=dataxml, headers={'accept': 'application/xml',
+                                                                                      'Content-Type': 'application/xml'})
+    if answer.status_code == 200:
+        return answer
+    elif answer.status_code == 405:
+        raise RuntimeError('Invalid input')
