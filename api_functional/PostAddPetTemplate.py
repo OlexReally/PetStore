0943e43491fe06df2153ff01b5bfec38638df71e
OlexReally <sector1111@yandex.ru>
2017-12-18 15:58:37 +0200

    PetGetTags changed

--- api_functional/PostAddPetTemplate.py
+++ api_functional/PostAddPetTemplate.py
@@ -1,3 +1,4 @@
+import requests
 # --------------------------------------------------------------------------------- #
 # pet_id = 0
 
@@ -17,9 +18,9 @@
 # --------------------------------------------------------------------------------- #
 
 
-def get_post_pet_template(pet_id=0, pet_category_id=0, pet_category_name="",
-                          pet_name="", pet_photo_url="", pet_tag_id=0,
-                          pet_tag_name="", pet_status=""):
+def get_post_pet_template(pet_id="0", pet_category_id="0", pet_category_name="string",
+                          pet_name="doggie", pet_photo_url="string", pet_tag_id="0",
+                          pet_tag_name="string", pet_status="available"):
     post_pet_xml = '''<?xml version="1.0" encoding="UTF-8"?>
     <Pet>
         <id>''' + pet_id + '''</id>
@@ -41,3 +42,12 @@ def get_post_pet_template(pet_id=0, pet_category_id=0, pet_category_name="",
     </Pet>'''
 
     return post_pet_xml
+
+
+
+url = 'http://petstore.swagger.io/v2/pet'
+header = {'accept': 'application/xml', 'Content-Type': 'application/xml'}
+
+r = requests.post(url, data=get_post_pet_template(), headers=header)
+
+print(r.status_code)
