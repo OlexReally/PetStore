e27aa467738df6e9f4a10f464a770c70f39da5c4
OlexReally <sector1111@yandex.ru>
2017-12-19 16:14:17 +0200

    pet changed

--- api_functional/pet/pet.py
+++ api_functional/pet/pet.py
@@ -1,5 +1,5 @@
 from lxml import objectify
-from api_functional.rest_request.connector import *
+from api_functional.rest_requests.connector import *
 from data.pet import *
 
 # get pet by id
@@ -8,22 +8,55 @@ class Pet:
     __URL = "v2/pet/"
     __HEADERS_SINGLE = {'accept': 'application/xml'}
     __HEADERS_DOUBLE = {'accept': 'application/xml', 'Content-Type': 'application/xml'}
-    __pet_object = None
 
-    def __init__(self, url, pet):
-        # http://petstore.swagger.io/   +   v2/pet/
-        self.__URL = url + self.__URL + pet.id  # str(pet_id)
+    def __init__(self, url, pet_object):
+        self.__URL = url + self.__URL  # pet.id str(pet_id)
+        self.__pet_init(pet_object)
 
-        self.__pet_object = self.__pet_init(pet)
-
-        # self.r = get("http://petstore.swagger.io/v2/pet/" + str(pet_id))
-        # self.pet_response = get(self.__URL, self.__HEADERS)
-        # self.__pet_object = objectify.fromstring(self.pet_response.content)
-
-    def __pet_init(self, pet):
-        return objectify.fromstring(get(self.__URL, self.__HEADERS))
+    def __pet_init(self, pet_object):
+        self.__pet_id = pet_object.id.text
+        self.__pet_category_id = pet_object.category.id.text
+        self.__pet_category_name = pet_object.category.name.text
+        self.__pet_name = pet_object.name.text
+        self.__pet_photo_url = pet_object.photoUrls.photoUrl.text
+        self.__pet_tag_id = pet_object.tags.tag.id.text
+        self.__pet_tag_name = pet_object.tags.tag.name.text
+        self.__pet_status = pet_object.status.text
+        pass
 
     # ------------- getters ----------------
+    @property
+    def id(self):
+        return self.__pet_id
+
+    @property
+    def category_id(self):
+        return self.__pet_category_id
+
+    @property
+    def category_name(self):
+        return self.__pet_category_name
+
+    @property
+    def name(self):
+        return self.__pet_name
+
+    @property
+    def photo_url(self):
+        return self.__pet_photo_url
+
+    @property
+    def tag_id(self):
+        return self.__pet_tag_id
+
+    @property
+    def tag_name(self):
+        return self.__pet_tag_name
+
+    @property
+    def status(self):
+        return self.__pet_status
+
     # def id_tag(self):
     #     return self.response_object.id.text
     #
@@ -40,10 +73,10 @@ class Pet:
     #     return self.response_object.tags.text
     # ------------- getters ----------------
 
-
-    def __add(self, pet):  # post
-        objectify.fromstring(get(self.__URL, self.__HEADERS))
-        pass
+    # post not need
+    # def __add(self, pet):  # post
+    #     objectify.fromstring(get(self.__URL, self.__HEADERS))
+    #     pass
 
     def update_full(self, pet):  # put
         pass
@@ -57,9 +90,8 @@ class Pet:
     def update_by_id(self, pet):  # post by id
         pass
 
-    def delete(self, pet):  # delete
-        # delete __pet_object
-        pass
+    def delete(self):  # delete
+        Connector.delete(url, self.__H)
 
     def upload_image(self, pet):  # post uploadImage
         pass
