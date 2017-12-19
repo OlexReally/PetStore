a71fae2ad8d48dc653527e64a16051beb085b063
AndriiKravtsov <andrjusyk@gmail.com>
2017-12-19 13:44:39 +0200

    Add few tests

--- api_functional/GetPetsInfoByStatus.py
+++ api_functional/GetPetsInfoByStatus.py
@@ -2,13 +2,9 @@ from api_functional.Request import getapi
 from lxml import etree
 
 
-class GetPetsInfoByStatus:
-    def __init__(self, status, tag):
-        self.answer = getapi("http://petstore.swagger.io/v2/pet/findByStatus?status=" + str(status))
-        self.tag = tag
-
-    def get_tag_list(self):
-        tree = etree.XML(self.answer.content)
-        Pets = tree.xpath('.//Pet/' + self.tag)
-        for pet in Pets:
-            print(self.tag + ' = ', [pet.text])
+def get_tag_list(status, tag):
+    answer = getapi("http://petstore.swagger.io/v2/pet/findByStatus?status=" + str(status))
+    tree = etree.XML(answer.content)
+    Pets = tree.xpath('.//Pet/' + tag)
+    for pet in Pets:
+         print(tag + ' = ', [pet.text])
