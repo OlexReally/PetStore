7d234e2b6f182c2175166e297257edaaac4471f5
OlexReally <sector1111@yandex.ru>
2017-12-21 11:24:30 +0200

    fixture pet_provider

--- tests/pet/conftest.py
+++ tests/pet/conftest.py
@@ -1,27 +1,31 @@
 import pytest
-from lxml import objectify
+# from lxml import objectify
+#
+# CAT_PATH = '/PetStore/data/pet_func/rabbit.xml'
+# DOG_PATH = '/PetStore/data/pet_func/dog.xml'
+# RABBIT_PATH = '/PetStore/data/pet_func/rabbit.xml'
+#
+# # tests/pet_func/conftest.py
+#
+#
+# # pet_obj = objectify.fromstring(pet_data_provider)
+# @pytest.fixture()
+# def pet_rabbit_provider():
+#     # xml_tool = XML_Tool()
+#     return 0  # xml_tool.get_xml_from_file(file=RABBIT_PATH)
+#
+#
+# @pytest.fixture()
+# def pet_dog_provider():
+#     # xml_tool = XML_Tool()
+#     return 0  # xml_tool.get_xml_from_file(file=DOG_PATH)
+#
+#
+# @pytest.fixture()
+# def pet_cat_provider():
+#     #xml_tool = XML_Tool()
+#     return 0  # xml_tool.get_xml_from_file(file=CAT_PATH)
 
-CAT_PATH = '/PetStore/data/pet_func/rabbit.xml'
-DOG_PATH = '/PetStore/data/pet_func/dog.xml'
-RABBIT_PATH = '/PetStore/data/pet_func/rabbit.xml'
-
-# tests/pet_func/conftest.py
-
-
-# pet_obj = objectify.fromstring(pet_data_provider)
-@pytest.fixture()
-def pet_rabbit_provider():
-    # xml_tool = XML_Tool()
-    return 0  # xml_tool.get_xml_from_file(file=RABBIT_PATH)
-
-
-@pytest.fixture()
-def pet_dog_provider():
-    # xml_tool = XML_Tool()
-    return 0  # xml_tool.get_xml_from_file(file=DOG_PATH)
-
-
-@pytest.fixture()
-def pet_cat_provider():
-    #xml_tool = XML_Tool()
-    return 0  # xml_tool.get_xml_from_file(file=CAT_PATH)
+# @pytest.fixture(params=["smtp.gmail.com", "mail.python.org"])
+# def pet_provider(request):
+#     pass
