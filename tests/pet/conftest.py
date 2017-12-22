1942d5a7818820be6da2b8702a1aa4a9bc016d84
AndriiKravtsov <andrjusyk@gmail.com>
2017-12-22 12:56:18 +0200

    Add hook to conftest

--- tests/pet/conftest.py
+++ tests/pet/conftest.py
@@ -1,8 +1,26 @@
 import pytest
 from data.pet.pet_container import cat_pet, dog_pet, rabbit_pet, wrong_pet
 import logging
+import traceback
 
 LOG_LVL = logging.DEBUG
+PASS = 32
+FAIL = 33
+logging.addLevelName(PASS, 'PASS')
+logging.addLevelName(FAIL, 'FAIL')
+
+
+def passed(self, message, *args, **kws):
+    self.log(PASS, message, *args, **kws)
+
+
+def failed(self, message, *args, **kws):
+    self.log(FAIL, message, *args, **kws)
+
+
+logging.Logger.passed = passed
+logging.Logger.failed = failed
+log = logging.getLogger()
 
 
 @pytest.fixture(params=[cat_pet(), dog_pet(), rabbit_pet()])
@@ -28,7 +46,25 @@ def pytest_sessionstart():
 
 def pytest_runtest_setup(item):
     logging.info("====={}=====".format(item.name))
+    log.info(item._obj.__doc__)
 
 
 def pytest_runtest_teardown(item):
     logging.info("====={}=====".format(item.name))
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.failed:
+        tb = "".join(traceback.format_tb(call.excinfo.tb)[-5:])
+        if call.excinfo.type is not AssertionError:
+            log.error("Error \"{}\" occurred! Traceback: \n{}".format(call.excinfo.typename, tb))
+        else:
+            log.failed("Test {} failed! \n{}".format(item.name, tb))
+
+    elif rep.passed and rep.when == "call":
+        log.passed("Test {} passed.".format(item.name))
